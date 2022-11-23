5ff03fbf1d8dc59b0aa87d7b472a0957fa3771e7
Kamsi Orazulike <kamsi_orazulike@icloud.com>
2022-11-23 13:47:13 +0000

    updates

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,42 +1,42 @@
-import './index.scss'
-import { useRef, useState } from 'react'
-import AnimatedLetters from '../AnimatedLetters'
-import emailjs from '@emailjs/browser'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import "./index.scss";
+import { useRef, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import emailjs from "@emailjs/browser";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 // import contact from '../../assets/imgs/contactsign.png'
 
 const Contact = () => {
-  const [CharClass, setLetterClass] = useState('animate-text')
-  const form = useRef()
+  const [CharClass, setLetterClass] = useState("animate-text");
+  const form = useRef();
 
   setTimeout(() => {
-    setLetterClass('animate-text-hover')
-  }, 4000)
+    setLetterClass("animate-text-hover");
+  }, 4000);
 
   const sendEmail = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     emailjs
-      .sendForm('gmail', 'template_q145by3', form.current, 'rnmLytGS0mDYkzlp6')
+      .sendForm("gmail", "template_q145by3", form.current, "rnmLytGS0mDYkzlp6")
       .then(
         () => {
-          alert('Message successfully sent!')
-          window.location.reload(false)
+          alert("Message successfully sent!");
+          window.location.reload(false);
         },
         () => {
-          alert('Failed to send the message, please try again')
+          alert("Failed to send the message, please try again");
         }
-      )
-  }
+      );
+  };
 
   return (
     <>
-      <div className="container contact-page" id='contact'>
+      <div className="container contact-page" id="contact">
         <div className="text-zone">
           <h1>
             <AnimatedLetters
-              CharArray={['L', 'e', 't', 's', ' ', 'T', 'a', 'l', 'k', '!']}
+              CharArray={["L", "e", "t", "s", " ", "T", "a", "l", "k", "!"]}
               CharClass={CharClass}
               indx={15}
             />
@@ -47,13 +47,13 @@ const Contact = () => {
             </p>
             <p>
               You can also email me at &nbsp;
-              <span
+              <a
                 href="mailto:kamsi_orazulike@icloud.com"
                 className="hover-link grow"
               >
                 <FontAwesomeIcon icon={faEnvelope} />
                 &nbsp;kamsi _orazulike@icloud.com
-              </span>
+              </a>
               &nbsp;or make use of the form below!
             </p>
           </h1>
@@ -98,7 +98,7 @@ const Contact = () => {
         {/* <img src={contact} alt="contactMeme" className="imageMeme" /> */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
