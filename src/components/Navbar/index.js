b8cb221e589da747c3b1a62f10a0aae8f8c92302
Kamsi Orazulike <kamsi_orazulike@icloud.com>
2022-12-21 16:28:39 +0000

    updates

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -42,7 +42,7 @@ const Navbar = () => {
             offset={-70}
             duration={500}
           >
-            About Me
+            About
           </LinkScroll>
         </div>
       </div>
