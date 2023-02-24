efac00c9fdf30f8a0e51c8f9ec35492e2c55cfb0
kamsiOrazulike <kamsi_orazulike@icloud.com>
2023-02-24 14:35:59 +0100

    updates

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -7,12 +7,13 @@ const PageNotFound = () => {
     <>
       <div className="no-page-found">
         <h1>Oops! This page doesn't exist yet! </h1>
-        <img src={bigBaby} alt="thinkingBaby" />
         <div className="link-group">
           <Link to="/" className="button-link">
             Head to home?
           </Link>
         </div>
+
+        <img src={bigBaby} alt="thinkingBaby" />
       </div>
     </>
   );
